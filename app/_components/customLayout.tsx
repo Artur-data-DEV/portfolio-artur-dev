0aef98b0894f8a4b13f9c1d549837409cd7294da
Artur Campos <arturcamposba99@gmail.com>
2024-05-29 12:31:10 -0300

    feat: adding framermotion on components

---
+++ app/_components/customLayout.tsx
@@ -0,0 +1,11 @@
+const CustomLayout = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <div className={`${className}`}>{children}</div>;
+};
+
+export default CustomLayout;
--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
+"use client";
 import CustomLink from "./custom-link";
 import Logo from "./logo";
+import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
@@ -12,22 +14,43 @@ const Navbar = () => {
         <CustomLink href="/projects" title={"Projetos"} className="mx-4" />
         <CustomLink href="/contact" title={"Contato"} className="ml-4" />
       </nav>
-      <nav>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
+      <nav className="flex flex-wrap items-center justify-center">
+        <motion.a
+          href="https://www.instagram.com"
+          target="_blank"
+          className="mr-3 w-6"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <FaInstagram size={24} />
+        </motion.a>
+        <motion.a
+          href="https://www.github.com/Artur-data-DEV"
+          target="_blank"
+          className="mr-3 w-6"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <FaGithub size={24} />
+        </motion.a>
+        <motion.a
+          href="/"
+          target="_blank"
+          className="mr-3 w-6"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <FaLinkedin size={24} />
+        </motion.a>
+        <motion.a
+          href="/"
+          target="_blank"
+          className="mr-3 w-6"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <FaWhatsapp size={24} />
+        </motion.a>
       </nav>
 
       <div className="absolute left-1/2 top-2 translate-x-[-50%]">
